15332cff3612df7753a57e5cc02b547cd2d71a68
hesti <hesti@LAPTOP-1781LM69>
2021-10-10 12:02:43 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.number = new System.Windows.Forms.TextBox();
+            this.temperature = new System.Windows.Forms.TextBox();
+            this.date = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 62);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "学籍番号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 178);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "体温";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("ＭＳ 明朝", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(117, 294);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "決定";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // number
+            // 
+            this.number.Location = new System.Drawing.Point(93, 84);
+            this.number.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(164, 25);
+            this.number.TabIndex = 3;
+            // 
+            // temperature
+            // 
+            this.temperature.Location = new System.Drawing.Point(93, 201);
+            this.temperature.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.temperature.Name = "temperature";
+            this.temperature.Size = new System.Drawing.Size(164, 25);
+            this.temperature.TabIndex = 4;
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Location = new System.Drawing.Point(14, 9);
+            this.date.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(0, 18);
+            this.date.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 374);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.temperature);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "体温入力";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox number;
+        private System.Windows.Forms.TextBox temperature;
+        private System.Windows.Forms.Label date;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            date.Text = dt.ToShortDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Excel.Application app = new Excel.Application();
+            app.DisplayAlerts = false;
+            app.Visible = false;
+            Excel.Workbooks excelBooks = app.Workbooks;
+            Excel.Workbook excelBook = excelBooks.Open("C:\\Users\\hesti\\source\\repos\\WindowsFormsApp1\\testb"); //指定された場所のExcelファイルを開く
+            Excel.Worksheet sheet = app.Worksheets["sheet1"];
+
+            try{
+
+                string sday=null;
+
+                int count = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    if (count == 2) sday += date.Text[i];
+                    if (date.Text[i] == '/') count++;
+                }
+                
+                var tempera = temperature.Text;
+                int iday = int.Parse(sday);
+                sheet.Cells[number.Text, iday] = tempera; //指定されたセルに体温データを保存する
+
+                excelBook.Save(); //Excelファイルの上書き保存
+                number.Text = null;
+                temperature.Text = null;
+                MessageBox.Show("保存しました");
+            }
+            catch
+            {
+                throw;
+            }
+            finally{
+                app.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelBook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+            }
+        }
+    }
+}
